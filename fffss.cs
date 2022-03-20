efa737dc07e7078a098b332fea36d20d9cfa8c4a
Lin Yu-Chieh (Victor) <victorlin@gapps.ntust.edu.tw>
2022-03-20 17:24:27 +0800

    Add backup solution

--- fffss.cs
+++ fffss.cs
@@ -20,94 +20,114 @@ namespace ffmpegFileSizeSplit
             streamwriter.AutoFlush = true;
             foreach (var file in files)
             {
-                var p = new Process();
-                p.StartInfo.UseShellExecute = false;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.FileName = "ffprobe.exe";
-                p.StartInfo.Arguments = $"-v error -select_streams v:0 -show_entries format=size -of default=noprint_wrappers=1:nokey=1 \"{file}\"";
+                var p = NewProcess("ffprobe.exe", $"-v error -select_streams v:0 -show_entries format=size -of default=noprint_wrappers=1:nokey=1 \"{file}\"");
                 p.Start();
                 double size = Convert.ToInt64(p.StandardOutput.ReadToEnd());
                 p.WaitForExit();
-                /*p = new Process();
-                p.StartInfo.UseShellExecute = false;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.FileName = "ffprobe.exe";
-                p.StartInfo.Arguments = $"-v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{file}\"";
+                p = NewProcess("ffprobe.exe", $"-v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{file}\"");
                 p.Start();
                 double duration = Convert.ToDouble(p.StandardOutput.ReadToEnd());
-                p.WaitForExit();*
-                int limit = Convert.ToInt32(args[1]);
-                int segCount = (int)Math.Ceiling(duration / ((duration / size) * limit));
-                limit = (int)Math.Ceiling(duration / segCount);*/
+                p.WaitForExit();
                 int limit = Convert.ToInt32(args[1]);
                 int segCount = (int)Math.Ceiling(size/limit);
                 limit = (int)Math.Ceiling(size/segCount);
 
-
                 var name = Path.GetFileNameWithoutExtension(file);
                 double dur = 0;
 
+                bool useAlt = false;
                 for (int i = 0; i < segCount; i++)
                 {
-                    p = new Process();
-                    p.StartInfo.UseShellExecute = false;
-                    p.StartInfo.RedirectStandardOutput = true;
-                    p.StartInfo.RedirectStandardError = true;
-                    p.StartInfo.FileName = "ffmpeg.exe";
-                    //p.StartInfo.Arguments = $"-y -i \"{file}\" -map 0 -c:v copy -c:a copy -c:s mov_text -segment_time {limit} -f segment -reset_timestamps 1 \"{args[2]}\\{name} PART %1d.mp4\" -loglevel error";
+                    useAlt = false;
                     string ss = "";
                     if(i>0)
                     {
-                        var probe = new Process();
-                        probe.StartInfo.UseShellExecute = false;
-                        probe.StartInfo.RedirectStandardOutput = true;
-                        probe.StartInfo.FileName = "ffprobe.exe";
-                        probe.StartInfo.Arguments = $"-v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{args[2]}\\{name} PART{i-1}.mp4\"";
+                        var probe = NewProcess("ffprobe.exe", $"-v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{args[2]}\\{name} PART{i - 1}.mp4\"");
                         probe.Start();
                         dur += Convert.ToDouble(probe.StandardOutput.ReadToEnd())-4;
                         ss = " -ss "+dur;
                         probe.WaitForExit();
+                        probe.Dispose();
                     }
-                    p.StartInfo.Arguments = $"-y{ss} -i \"{file}\" -map 0 -c:v copy -c:a copy -c:s mov_text -fs {limit} -reset_timestamps 1 \"{args[2]}\\{name} PART{i}.mp4\" -loglevel error";
-                    p.Start();
-                    var result = p.StandardOutput.ReadToEnd();
-                    var error = p.StandardError.ReadToEnd();
-                    p.WaitForExit();
-                    if (error!="")
+                    string result="", error="";
+                    Run(useAlt, file, name, limit, args[2], ref result, ref error, ss, i);
+                    if(i==1)
                     {
-                        p = new Process();
-                        p.StartInfo.UseShellExecute = false;
-                        p.StartInfo.RedirectStandardOutput = true;
-                        p.StartInfo.RedirectStandardError = true;
-                        p.StartInfo.FileName = "ffmpeg.exe";
-                        p.StartInfo.Arguments = $"-y{ss} -i \"{file}\" -map 0 -c:v copy -c:a copy -c:s dvdsub -fs {limit} -reset_timestamps 1 \"{args[2]}\\{name} PART{i}.mp4\" -loglevel error";
-                        p.Start();
-                        result = p.StandardOutput.ReadToEnd();
-                        error = p.StandardError.ReadToEnd();
-                        p.WaitForExit();
-                    }
-                    if (error!="")
-                    {
-                        p = new Process();
-                        p.StartInfo.UseShellExecute = false;
-                        p.StartInfo.RedirectStandardOutput = true;
-                        p.StartInfo.RedirectStandardError = true;
-                        p.StartInfo.FileName = "ffmpeg.exe";
-                        p.StartInfo.Arguments = $"-y{ss} -i \"{file}\" -c copy -fs {limit} -reset_timestamps 1 \"{args[2]}\\{name} PART{i}.mp4\" -loglevel error";
-                        p.Start();
-                        result = "Subtitle Skipped "+p.StandardOutput.ReadToEnd();
-                        error = p.StandardError.ReadToEnd();
-                        p.WaitForExit();
+                        var probe = NewProcess("ffprobe.exe", $"-v error -select_streams v:0 -show_entries format=size -of default=noprint_wrappers=1:nokey=1 \"{args[2]}\\{name} PART0.mp4\"");
+                        probe.Start();
+                        double d0 = Convert.ToDouble(probe.StandardOutput.ReadToEnd());
+                        probe = NewProcess("ffprobe.exe", $"-v error -select_streams v:0 -show_entries format=size -of default=noprint_wrappers=1:nokey=1 \"{args[2]}\\{name} PART1.mp4\"");
+                        probe.Start();
+                        double d1 = Convert.ToDouble(probe.StandardOutput.ReadToEnd());
+                        probe.WaitForExit();
+                        probe.Dispose();
+                        if (Math.Abs(d1-d0)>size*0.1)
+                        {
+                            useAlt = true;
+                            break;
+                        }
                     }
                     if (result == "") result = "Processed";
                     streamwriter.WriteLine($"{DateTime.Now},\"{name} - {i}\",{result},{error}");
                     Console.WriteLine($"{name} PART {i} {result} {error}");
+                }
+                if(useAlt)
+                {
+                    limit = Convert.ToInt32(args[1]);
+                    segCount = (int)Math.Ceiling(duration / ((duration / size) * limit));
+                    limit = (int)Math.Ceiling(duration / segCount);
+                    string result = "", error = "";
+                    Run(useAlt, file, name, limit, args[2], ref result, ref error);
+                    streamwriter.WriteLine($"{DateTime.Now},\"{name}\",{result},{error}");
+                    Console.WriteLine($"{name} {result} {error}");
                 }
                 p.Dispose();
             }
             streamwriter.Close();
             filestream.Close();
             Console.WriteLine("END");
+        }
+        static Process NewProcess(string fileName, string arg)
+        {
+            var p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.FileName = fileName;
+            p.StartInfo.Arguments = arg;
+            return p;
+        }
+        static void Run(bool alt, string file, string name, int limit, string outDir, ref string result, ref string error, string ss = "", int i=0)
+        {
+            var p = NewProcess("ffmpeg.exe", alt?
+                $"-y -i \"{file}\" -map 0 -c:v copy -c:a copy -c:s mov_text -segment_time {limit} -f segment -reset_timestamps 1 \"{outDir}\\{name} PART%1d.mp4\" -loglevel error" : 
+                $"-y{ss} -i \"{file}\" -map 0 -c:v copy -c:a copy -c:s mov_text -fs {limit} -reset_timestamps 1 \"{outDir}\\{name} PART{i}.mp4\" -loglevel error");
+            p.Start();
+            result = (alt? "Warning Alt method used - please double check file size" :"") + p.StandardOutput.ReadToEnd();
+            error = p.StandardError.ReadToEnd();
+            p.WaitForExit();
+            if (error != "")
+            {
+                p = NewProcess("ffmpeg.exe", alt?
+                    $"-y -i \"{file}\" -map 0 -c:v copy -c:a copy -c:s dvdsub -segment_time {limit} -f segment -reset_timestamps 1 \"{outDir}\\{name} PART%1d.mp4\" -loglevel error":
+                    $"-y{ss} -i \"{file}\" -map 0 -c:v copy -c:a copy -c:s dvdsub -fs {limit} -reset_timestamps 1 \"{outDir}\\{name} PART{i}.mp4\" -loglevel error");
+                p.Start();
+                result = (alt ? "Warning Alt method used - please double check file size" : "") + p.StandardOutput.ReadToEnd();
+                error = p.StandardError.ReadToEnd();
+                p.WaitForExit();
+            }
+            if (error != "")
+            {
+                p = NewProcess("ffmpeg.exe", alt?
+                    $"-y -i \"{file}\" -c copy -segment_time {limit} -f segment -reset_timestamps 1 \"{outDir}\\{name} PART%1d.mp4\" -loglevel error":
+                    $"-y{ss} -i \"{file}\" -c copy -fs {limit} -reset_timestamps 1 \"{outDir}\\{name} PART{i}.mp4\" -loglevel error");
+                p.Start();
+                result = (alt ? "Warning Alt method used - please double check file size _ " : "") + "Subtitle Skipped " + p.StandardOutput.ReadToEnd();
+                error = p.StandardError.ReadToEnd();
+                p.WaitForExit();
+            }
+            p.Dispose();
+            return;
         }
     }
 }
